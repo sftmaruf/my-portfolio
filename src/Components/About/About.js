efc60b3ad13ba33ba648ea5768d6f196af861726
sftmaruf <you@example.com>
2020-11-03 06:19:47 +0600

    add about

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,13 +4,18 @@ import './About.scss';
 
 const About = () => {
     return (
-        <div id = 'about' className='p-5 row'>
+        <div id='about' className='p-5 row'>
             <div className='col-xl-4 col-md-12 d-flex justify-content-end'>
-                <img className = 'about-image' src = {aboutImage} alt=""/>
+                <img className='about-image' src={aboutImage} alt="" />
             </div>
             <div className='col-xl-4 col-md-12 d-flex flex-column justify-content-center about-description'>
                 <h2>About</h2>
-                <p className = 'mt-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore iusto facere, nam deserunt consectetur minus vero nihil aliquid eveniet quibusdam, eius earum delectus placeat repudiandae odit et perferendis dolores itaque ex, eligendi vel impedit deleniti libero voluptatem! Quisquam, tenetur quod.</p>
+                <p className='mt-4'>In my memories, I remember when I was a Tech addicted I was thinking about how the software is made for tech devices. And then after immense searching for the first time, I become familiar with the term 'Programming Language'.
+                <br /><br/>
+                This enthusiasm for technology brings me to the software engineering field. And now it became my passion and also my career for the rest of my life.
+                <br /><br/>
+                Still, there's certainly yet more to learn and I commit to myself for learning lifelong.
+                </p>
             </div>
             <div className="col-xl-4 col-md-12 d-flex flex-column justify-content-center about-extra-description">
                 <h5>Let's collaborate</h5>
